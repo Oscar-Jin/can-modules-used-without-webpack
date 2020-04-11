62c5a687e6f03a1a0e784efd5940bbb4ce2c3d89
Zhiren Jin <zhirenjin@Zhirens-iMac.local>
2020-04-11 23:32:10 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import { describeOscar } from "./utils.js";
+
+
+let p1 = document.createElement('p')
+p1.innerText = describeOscar()
+
+document.body.appendChild(p1)
---
+++ utils.js
@@ -0,0 +1,3 @@
+export const describeOscar = () => {
+  return ("Oscar is a genious and he is the most significant person in 21st century")
+}
