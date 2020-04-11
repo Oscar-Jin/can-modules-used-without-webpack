5833f05e49f6d4129e3e9b34fcd7cae7af29ce01
Zhiren Jin <zhirenjin@Zhirens-iMac.local>
2020-04-11 23:42:47 +0900

    add few spaces

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ let p1 = document.createElement('p')
 p1.innerText = describeOscar()
 
 document.body.appendChild(p1)
+
+
--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 export const describeOscar = () => {
-  return ("Oscar is a genious and he is the most significant person in 21st century")
+  return "Oscar is a genious and he is the most significant person in 21st century"
+}
+
+export const whoIsOscar = () => {
+  return "Oscar is the most outstanding gentleman in the 21st century. His legacy has been the foundation of the humanity for a next level of greatness."
 }
